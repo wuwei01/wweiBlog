b6b43f6a1cc356cc384d40b8484c633d59a2cc37
wuwei <969502105@qq.com>
2021-05-24 09:19:12 +0800

    添加redis配置类方便使用redis进行数据缓存信息等

---
+++ src/main/java/io/github/wuwei01/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package io.github.wuwei01.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @ClassName：RedisConfig
+ * @Description：Redis配置类
+ * @Author：wuwei
+ * @Date：2021年5月24日09:12:10
+ * @Versiion：1.0
+ */
+@Configuration //当前类为配置类
+public class RedisConfig {
+    @Bean //redisTemplate注入到Spring容器
+    public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory factory){
+        RedisTemplate<String,String> redisTemplate=new RedisTemplate<>();
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        redisTemplate.setConnectionFactory(factory);
+        //key序列化
+        redisTemplate.setKeySerializer(redisSerializer);
+        //value序列化
+        redisTemplate.setValueSerializer(redisSerializer);
+        //value hashmap序列化
+        redisTemplate.setHashKeySerializer(redisSerializer);
+        //key hashmap序列化
+        redisTemplate.setHashValueSerializer(redisSerializer);
+        return redisTemplate;
+    }
+}
