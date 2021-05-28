fa827df31914939b78ef804f74a036d6f90d34ad
wuwei <969502105@qq.com>
2021-05-28 09:31:46 +0800

    添加fastdfs依赖用来支持文件服务

---
+++ src/main/java/io/github/wuwei01/VueBlogApplication.java
@@ -0,0 +1,17 @@
+package io.github.wuwei01;
+
+import com.github.tobato.fastdfs.FdfsClientConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.context.annotation.Import;
+import org.springframework.jmx.support.RegistrationPolicy;
+
+@Import(FdfsClientConfig.class)
+@EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
+@SpringBootApplication
+public class VueBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VueBlogApplication.class,args);
+    }
+}
