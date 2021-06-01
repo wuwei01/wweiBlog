2d614e0a7ab1ee4a7742d47910c109fdda7783dd
wuwei <969502105@qq.com>
2021-06-01 14:21:07 +0800

    添加记录日志注解，方便记录一下操作日志或者订单日志

--- src/main/java/io/github/wuwei01/VueBlogApplication.java
+++ src/main/java/io/github/wuwei01/VueBlogApplication.java
@@ -1,6 +1,7 @@
 package io.github.wuwei01;
 
 import com.github.tobato.fastdfs.FdfsClientConfig;
+import com.mzt.logapi.starter.annotation.EnableLogRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableMBeanExport;
@@ -10,6 +11,7 @@ import org.springframework.jmx.support.RegistrationPolicy;
 @Import(FdfsClientConfig.class)
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
 @SpringBootApplication
+@EnableLogRecord(tenant = "io.github.wuwei01")
 public class VueBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(VueBlogApplication.class,args);
---
+++ src/main/java/io/github/wuwei01/service/OrderService.java
@@ -0,0 +1,48 @@
+package io.github.wuwei01.service;
+
+import com.mzt.logapi.starter.annotation.LogRecordAnnotation;
+
+public class OrderService {
+
+//    @LogRecordAnnotation(
+//            fail = "创建订单失败，失败原因：「{{#_errorMsg}}」",
+//            success = "{{#order.purchaseName}}下了一个订单,购买商品「{{#order.productName}}」,下单结果:{{#_ret}}",
+//            prefix = LogRecordType.ORDER, bizNo = "{{#order.orderNo}}")
+//    public boolean createOrder(Order order) {
+//        log.info("【创建订单】orderNo={}", order.getOrderNo());
+//        // db insert order
+//        return true;
+//    }
+
+
+//    @LogRecordAnnotation(success = "{{#order.purchaseName}}下了一个订单,购买商品「{{#order.productName}}」,下单结果:{{#_ret}}",
+//            prefix = LogRecordType.ORDER, bizNo = "{{#order.orderNo}}")
+//    public boolean createOrder(Order order) {
+//        log.info("【创建订单】orderNo={}", order.getOrderNo());
+//        // db insert order
+//        return true;
+//    }
+
+//    @LogRecordAnnotation(
+//            fail = "创建订单失败，失败原因：「{{#_errorMsg}}」",
+//            category = "MANAGER",
+//            success = "{{#order.purchaseName}}下了一个订单,购买商品「{{#order.productName}}」,下单结果:{{#_ret}}",
+//            prefix = LogRecordType.ORDER, bizNo = "{{#order.orderNo}}")
+//    public boolean createOrder(Order order) {
+//        log.info("【创建订单】orderNo={}", order.getOrderNo());
+//        // db insert order
+//        return true;
+//    }
+
+//    @LogRecordAnnotation(
+//            fail = "创建订单失败，失败原因：「{{#_errorMsg}}」",
+//            category = "MANAGER_VIEW",
+//            detail = "{{#order.toString()}}",
+//            success = "{{#order.purchaseName}}下了一个订单,购买商品「{{#order.productName}}」,下单结果:{{#_ret}}",
+//            prefix = LogRecordType.ORDER, bizNo = "{{#order.orderNo}}")
+//    public boolean createOrder(Order order) {
+//        log.info("【创建订单】orderNo={}", order.getOrderNo());
+//        // db insert order
+//        return true;
+//    }
+}
